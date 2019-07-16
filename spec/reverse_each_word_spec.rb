eeeaf17936323a4f781eab862b354ca501f860bd
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-16 13:06:55 +0000

    Automatically backed up by Learn

--- spec/reverse_each_word_spec.rb
+++ spec/reverse_each_word_spec.rb
@@ -15,4 +15,5 @@ describe '#reverse_each_word' do
     expect_any_instance_of(Array).to receive(:collect).and_call_original
     reverse_each_word("Verifying that collect is being called.")
   end
+  
 end
